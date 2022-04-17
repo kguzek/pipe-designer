4f1c455c8a4ed7a0b8984eadfded710a8017ccc4
Konrad (PC) <konrad@guzek.uk>
2022-04-18 00:00:52 +0200

    it all goes downhill from here...

---
+++ js/index.js
@@ -0,0 +1,259 @@
+import validatePipeSelection, { AssertionError } from "./inputValidator.js";
+
+const OPTIONS = $("#options tr td");
+const GRID = $("#grid tr td");
+
+let currentlySelected = null;
+let currentlyHoveringElement = null;
+
+const pipeOrientation = {
+  cross: 0,
+  T: 0,
+  straight: 0,
+  L: 0,
+};
+
+/** The elements that the user has placed into the grid. */
+const userDesign = {};
+
+const disabledEdges = { columns: {}, rows: {} };
+const enabledEdges = { columns: {}, rows: {} };
+
+/** Sets the element's rotation in unit `grad`.
+ *  E.g. 100grad = 90deg
+ * 			 200grad = 180deg
+ * 			 400grad = 360deg
+ */
+function setRotation(elem, forceAngle) {
+  if (!elem) return;
+  const angleInGrad = forceAngle ?? pipeOrientation[currentlySelected];
+  elem.css("transform", `rotate(${angleInGrad}grad)`);
+}
+
+function selectPipeOption({ currentTarget }) {
+  if (!currentTarget?.id) return;
+  const clicked = currentTarget.id.split("-")[1];
+  if (clicked === currentlySelected) {
+    return void resetSelection();
+  }
+  resetSelection();
+  currentlySelected = clicked;
+  $("#" + currentTarget.id).addClass("selected");
+}
+
+function getImgElem(elemTarget) {
+  const position = elemTarget?.id?.at(-1);
+  if (!position) return;
+  return $("#pipe-" + position);
+}
+
+/** Gets the image filename of the pipe element. Defaults to the currently selected element. */
+function getImageFilename(elementName) {
+  return `images/pipe-${elementName ?? currentlySelected}.png`;
+}
+
+/** Parses the filename and finds the raw element name. */
+function getElementName(filename) {
+  if (!filename) return;
+  return filename.match(/images\/pipe-(\w+).png/)[1];
+}
+
+function hoverPipePosition({ target, currentTarget }, hoverOut) {
+  const cellElement = $(target);
+  const imgElem = getImgElem(currentTarget);
+  if (!imgElem) return;
+
+  if (hoverOut) {
+    currentlyHoveringElement = null;
+  }
+
+  const previousSelection = userDesign[currentTarget.id];
+
+  if (hoverOut) {
+    cellElement.removeClass("bg-light");
+    cellElement.addClass("bg-white");
+    imgElem.css("opacity", "1");
+    if (previousSelection) {
+      imgElem.attr("src", getImageFilename(previousSelection.elementName));
+      setRotation(imgElem, previousSelection.orientation);
+    } else {
+      imgElem.removeAttr("src");
+    }
+    return;
+  }
+  cellElement.addClass("bg-light");
+  cellElement.removeClass("bg-white");
+  currentlyHoveringElement = imgElem;
+  if (!currentlySelected || currentlySelected === "delete") {
+    return;
+  }
+  imgElem.attr("src", getImageFilename());
+  imgElem.css("opacity", "0.3");
+  setRotation(imgElem);
+}
+
+/** Removes the rules that this position imposes if no other position also imposes them. */
+function removePipeConditions(position) {
+  // Iterate through the enabled and disabled rules set by this position
+  for (const obj of [enabledEdges, disabledEdges]) {
+    // Iterate through both the set column and row rules
+    for (const property in obj) {
+      // Iterate through the individual columns/rows that are imposed
+      for (const attr in obj[property]) {
+        // Iterate through each position that imposes this condition
+        for (const pos of obj[property][attr]) {
+          if (pos !== position) continue;
+          // Remove the position from the array of positions imposing this condition
+          obj[property][attr].splice(obj[property][attr].indexOf(pos));
+          // Remove the condition if no pipe element position imposes it
+          // (Technically not needed but saves future memory and lookup speed)
+          if (obj[property][attr].length === 0) {
+            delete obj[property][attr];
+          }
+        }
+      }
+    }
+  }
+}
+
+/** Event handler for when a grid position is clicked. */
+function selectPipePosition({ currentTarget }) {
+  const imgElem = getImgElem(currentTarget);
+  if (!imgElem) return;
+  if (!currentlySelected) {
+    // User clicks grid element with no pipe option selected
+    const elementName = getElementName(imgElem.attr("src"));
+    if (elementName) {
+      handleRotate(false, elementName);
+      userDesign[currentTarget.id].orientation = pipeOrientation[elementName];
+    }
+    return;
+  }
+  if (currentlySelected === "delete") {
+    if (userDesign[currentTarget.id]) {
+      delete userDesign[currentTarget.id];
+      imgElem.removeAttr("src");
+      removePipeConditions(currentTarget.id);
+    }
+    return;
+  }
+  const orientation = pipeOrientation[currentlySelected];
+  try {
+    const { enabled, disabled } = validatePipeSelection(
+      currentlySelected,
+      orientation,
+      currentTarget.id,
+      { enabledEdges, disabledEdges }
+    );
+    if (userDesign[currentTarget.id]) {
+      removePipeConditions(currentTarget.id);
+    }
+    for (const [obj, values] of [
+      [enabledEdges, enabled],
+      [disabledEdges, disabled],
+    ]) {
+      for (const property in values) {
+        for (const attr of values[property]) {
+          if (!obj[property][attr]) {
+            obj[property][attr] = [];
+          }
+          obj[property][attr].push(currentTarget.id);
+        }
+      }
+    }
+  } catch (e) {
+    if (!(e instanceof AssertionError)) {
+      throw e;
+    }
+    errorAnimation($(currentTarget), "invalid-edge-" + e.assertedEdge);
+    return;
+  }
+
+  imgElem.attr("src", getImageFilename());
+  imgElem.css("opacity", "1");
+  setRotation(imgElem, orientation);
+  userDesign[currentTarget.id] = {
+    elementName: currentlySelected,
+    orientation,
+  };
+}
+
+/** Makes the edge flash red the specified number of times. */
+function errorAnimation(element, className, numFlashes = 2) {
+  function flash() {
+    element.toggleClass(className);
+  }
+
+  // Iterate twice for each flash; turn red and revert back.
+  for (let stage = 0; stage < numFlashes * 2; stage++) {
+    // 250 ms interval total between each flash sequence
+    setTimeout(flash, stage * 125);
+  }
+}
+
+function resetSelection(event) {
+  if (event) {
+    // Don't reset the selection if a table element is clicked
+    if (event.currentTarget !== event.target && event.target.id !== "root") {
+      return;
+    }
+  }
+  currentlySelected = null;
+  OPTIONS.removeClass("selected");
+}
+
+function getEventKey(e) {
+  const key = e?.originalEvent?.key;
+  return key;
+}
+
+function handleRotate(shiftIsHeld = false, optionOverride) {
+  if (optionOverride === undefined) optionOverride = currentlySelected;
+  if (!optionOverride) return;
+  let orientation = pipeOrientation[optionOverride];
+  if (shiftIsHeld) {
+    // Rotate in opposite direction if user holding the shift key
+    orientation -= 100;
+    if (orientation < 0) {
+      orientation = 300;
+    }
+  } else {
+    orientation += 100;
+    if (orientation > 300) {
+      orientation = 0;
+    }
+  }
+  pipeOrientation[optionOverride] = orientation;
+  const elem = $("#option-" + optionOverride);
+  setRotation(elem, orientation);
+  if (currentlyHoveringElement) {
+    // Update the pipe option preview if hovering over the grid
+    setRotation(currentlyHoveringElement, orientation);
+    if (currentlySelected) {
+      currentlyHoveringElement.css("opacity", "0.3");
+    }
+  }
+}
+
+function handleKeyDown(e) {
+  switch (getEventKey(e)) {
+    case "Escape":
+      resetSelection();
+      return;
+    case "r":
+      handleRotate();
+      return;
+    case "R":
+      handleRotate(true);
+      return;
+    default:
+      return;
+  }
+}
+
+OPTIONS.click(selectPipeOption);
+GRID.click(selectPipePosition).hover(
+  (event) => hoverPipePosition(event, false),
+  (event) => hoverPipePosition(event, true)
+);
+$(document).click(resetSelection).keydown(handleKeyDown);
---
+++ js/inputValidator.js
@@ -0,0 +1,117 @@
+const NON_CONNECTIVE_EDGES = {
+  cross: [],
+  T: [0],
+  straight: [0, 2],
+  L: [0, 1],
+};
+
+export class AssertionError extends Error {
+  constructor(isRequired, rowOrColumn, assertedEdge) {
+    super(
+      `${
+        isRequired ? "Required" : "Disabled"
+      } ${rowOrColumn}: edge ${assertedEdge}.`
+    );
+    this.name = "AssertionError";
+    this.assertedEdge = assertedEdge;
+  }
+}
+
+export default function validatePipeSelection(
+  elementName,
+  orientation,
+  elementPosition,
+  { enabledEdges, disabledEdges }
+) {
+  function assertState(value, isColumn, enabled, assertedEdge) {
+    let rowOrColumn = "row";
+    let objToCompareAgainst;
+    let arrayToModify;
+    if (isColumn) {
+      rowOrColumn = "column";
+      if (enabled) {
+        objToCompareAgainst = disabledEdges.columns;
+        arrayToModify = enabledColumns;
+      } else {
+        objToCompareAgainst = enabledEdges.columns;
+        arrayToModify = disabledColumns;
+      }
+    } else {
+      if (enabled) {
+        objToCompareAgainst = disabledEdges.rows;
+        arrayToModify = enabledRows;
+      } else {
+        objToCompareAgainst = enabledEdges.rows;
+        arrayToModify = disabledRows;
+      }
+    }
+    const positionsArray = objToCompareAgainst[value];
+    if (
+      positionsArray?.length > 1 ||
+      (positionsArray?.length === 1 &&
+        positionsArray?.shift() !== elementPosition)
+    )
+      throw new AssertionError(!enabled, rowOrColumn, assertedEdge);
+
+    arrayToModify.push(value);
+  }
+
+  const position = parseInt(elementPosition.split("-")[1]);
+  // Account for the element's rotation
+  const relativeEdges = NON_CONNECTIVE_EDGES[elementName].map(
+    (edge) => (edge + orientation / 100) % 4
+  );
+  const disabledRows = [];
+  const disabledColumns = [];
+
+  const enabledRows = [];
+  const enabledColumns = [];
+
+  // Make the column data format more intuitive, 1-6 instead of 1, 2 | 4, 5 | 7, 8
+  const columnOffset = Math.floor((position - 1) / 3);
+
+  // Top edge disabled
+  if (position > 3) {
+    if (relativeEdges.includes(0)) {
+      assertState(position - 3, false, false, 0);
+    } else {
+      assertState(position - 3, false, true, 0);
+    }
+  }
+
+  // Right edge disabled
+  if (position % 3 !== 0) {
+    if (relativeEdges.includes(1))
+      assertState(position - columnOffset, true, false, 1);
+    else {
+      assertState(position - columnOffset, true, true, 1);
+    }
+  }
+
+  // Bottom edge disabled
+  if (position < 7) {
+    if (relativeEdges.includes(2)) {
+      assertState(position, false, false, 2);
+    } else {
+      assertState(position, false, true, 2);
+    }
+  }
+
+  // Left edge disabled
+  if (position % 3 !== 1) {
+    if (relativeEdges.includes(3)) {
+      assertState(position - 1 - columnOffset, true, false, 3);
+    } else {
+      assertState(position - 1 - columnOffset, true, true, 3);
+    }
+  }
+
+  // console.log("Enabled rows:", enabledRows);
+  // console.log("Enabled columns:", enabledColumns);
+  // console.log("Disabled rows:", disabledRows);
+  // console.log("Disabled columns:", disabledColumns);
+  return {
+    disabled: { rows: disabledRows, columns: disabledColumns },
+    enabled: { rows: enabledRows, columns: enabledColumns },
+  };
+}
